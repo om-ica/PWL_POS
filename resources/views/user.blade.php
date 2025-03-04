6fdfcc946d872e2286e82f7aee16ee11cbc8d803
Irsa Cahaya Widodo <irsacahaya17@gmail.com>
2025-03-04 10:58:35 +0700

    Praktikum 2.3 "Retreiving Aggregrates" Telah Selesai

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,16 +9,17 @@
 <body>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
+            {{-- <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>ID Level Pengguna</th> --}}
+            <th>Jumlah Pengguna</th>
         </tr>
             <tr>
-                <td>{{$data->user_id}}</td>
-                <td>{{$data->username}}</td>
+                <td>{{$data}}</td>
+                {{-- <td>{{$data->username}}</td>
                 <td>{{$data->nama}}</td>
-                <td>{{$data->level_id}}</td>
+                <td>{{$data->level_id}}</td> --}}
             </tr>
     </table>
 </body>
