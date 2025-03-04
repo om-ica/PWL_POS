db361a38958ec23fbe1685d106419597fb2ebefa
Irsa Cahaya Widodo <irsacahaya17@gmail.com>
2025-03-05 00:59:56 +0700

    Praktikum 2.4 "Retreiving or Creating Models" Telah Selesai

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,17 +9,16 @@
 <body>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            {{-- <th>ID</th>
+            <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th> --}}
-            <th>Jumlah Pengguna</th>
+            <th>ID Level Pengguna</th>
         </tr>
             <tr>
-                <td>{{$data}}</td>
-                {{-- <td>{{$data->username}}</td>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
                 <td>{{$data->nama}}</td>
-                <td>{{$data->level_id}}</td> --}}
+                <td>{{$data->level_id}}</td>
             </tr>
     </table>
 </body>
