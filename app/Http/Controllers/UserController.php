990e06008c5f6a3a20c8a25bf4419557cc6e6d08
Irsa Cahaya Widodo <irsacahaya17@gmail.com>
2025-03-04 10:14:40 +0700

    Praktikum 2 "Retrieving Single Models" Telah Selesai

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,9 @@ class UserController extends Controller
 {
         public function index() 
         {
-            $user = UserModel::find(1); 
+            $user = UserModel::findOr(20, ['username', 'nama'], function (){
+                abort(404);
+            });
             return view('user', ['data' => $user]);
         }
 }
